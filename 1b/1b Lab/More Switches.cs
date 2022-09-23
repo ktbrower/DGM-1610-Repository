44ad71b1c19060878536b56cc7366cbfb0a56f58
ktbrower <112342451+ktbrower@users.noreply.github.com>
2022-09-23 09:32:33 -0600

    Create More Switches.cs

---
+++ 1b/1b Lab/More Switches.cs
@@ -0,0 +1,33 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+	int day = 7;
+switch (day) 
+{
+  case 1:
+    Console.WriteLine("Monday");
+    break;
+  case 2:
+    Console.WriteLine("Tuesday");
+    break;
+  case 3:
+    Console.WriteLine("Wednesday");
+    break;
+  case 4:
+    Console.WriteLine("Thursday");
+    break;
+  case 5:
+    Console.WriteLine("Friday");
+    break;
+  case 6:
+    Console.WriteLine("Saturday");
+    break;
+  case 7:
+    Console.WriteLine("Sunday");
+    break;
+}
+	}
+}
