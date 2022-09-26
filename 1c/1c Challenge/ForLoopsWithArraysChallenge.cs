3bcb7a49983c2838033b8d038b6ac68fb45e2da6
ktbrower <112342451+ktbrower@users.noreply.github.com>
2022-09-26 10:33:25 -0600

    Update ForLoopsWithArraysChallenge.cs

--- 1c/1c Challenge/ForLoopsWithArraysChallenge.cs
+++ 1c/1c Challenge/ForLoopsWithArraysChallenge.cs
@@ -1,1 +1,13 @@
-
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+	string[]TSalbums = {"Taylor Swift","Fearless","Speak Now", "Red", "1989", "Reputation","Lover","Folklore","Evermore","Midnights"};
+		for (int i = 0; i < 10; i++) 
+		{
+  		Console.WriteLine(TSalbums[i]);
+		}
+	}
+}
