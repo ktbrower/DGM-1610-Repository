14c6bcada1fe003bc43e30037861343dd0904c68
Katherine Brower <ktbrower101@gmail.com>
2022-11-02 17:05:54 -0600

    working on Lab 5 for reals

---
+++ 5/Real 5 Lab/5 Lab Final/Assets/TriggerEventsBehaviour.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.Events;
+public class TriggerEventsBehaviour : MonoBehaviour
+{
+    public UnityEvent triggerEnterEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+    }
+}
