d84df7d3b3fbaf2d25014b4ea1cf60ef0f51b1a0
Katherine Brower <ktbrower101@gmail.com>
2022-11-14 21:18:04 -0700

    Create Lab 6 Game

---
+++ 6/6 Lab/6 Lab/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+
+{
+    public float horizontalInput;
+    public float speed = 10;
+    public float xRange =47;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    
+    {
+        
+        horizontalInput = Input.GetAxis("Horizontal");
+        
+        transform.Translate(Vector3.right * (horizontalInput * Time.deltaTime * speed));
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
