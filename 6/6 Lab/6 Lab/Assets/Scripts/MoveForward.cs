e4cb110f563bca0faabec0291e3d0592b129f900
Katherine Brower <ktbrower101@gmail.com>
2022-11-17 14:55:16 -0700

    fixed blaster

--- 6/6 Lab/6 Lab/Assets/Scripts/MoveForward.cs
+++ 6/6 Lab/6 Lab/Assets/Scripts/MoveForward.cs
@@ -9,6 +9,6 @@ public class MoveForward : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * (Time.deltaTime * speed));
     }
 }
